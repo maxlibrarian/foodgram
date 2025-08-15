fc5c361c2f300b2ad4bdbc7aa93197d3400b3207
maxlibrarian <yc-user@epdgc0tickpsv7pdkl1a.auto.internal>
2025-08-15 14:05:24 +0000

    Add changes to Dockerfile

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -31,7 +31,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -104,7 +103,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 
