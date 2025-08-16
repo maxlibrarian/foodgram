f9ac1fd3697a3a3c62758f2115c642a3f1b6d3d9
Maksim Rumyantsev <max.pazzle@gmail.com>
2025-08-16 16:20:23 +0300

    Isort check

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Favorite, Ingredient, Recipe, RecipeIngredient, ShoppingCart, Tag
+from .models import (
+    Favorite, Ingredient, Recipe, RecipeIngredient, ShoppingCart, Tag
+)
 
 
 @admin.register(Tag)
