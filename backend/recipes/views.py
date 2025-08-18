5614e6214f71750b9b548d350358956f5f3658eb
Maksim Rumyantsev <max.pazzle@gmail.com>
2025-08-18 19:42:39 +0300

    First review iteration

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+
 from django.shortcuts import get_object_or_404, redirect
 from rest_framework import generics, viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated
